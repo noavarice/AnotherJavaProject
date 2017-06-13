3710832ba156aecf76723ab876d25daa346a98ed
noavarice <alex9razinkov@gmail.com>
2017-06-13 15:53:46 +0300

    changed main to test functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.company.models.SqlColumn;
+import com.company.models.SqlTable;
+import com.company.parsers.XmlParser;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.Collection;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, SAXException {
+        Collection<SqlTable> tables = XmlParser.fromFile("/home/alexrazinkov/Projects/Java/xml_test");
+        if (tables == null) {
+            System.out.println("No tables");
+            return;
+        }
+        for (SqlTable t : tables) {
+            SqlColumn[] columns = t.getTableColumns();
+            System.out.println(t.getTableName());
+            for (SqlColumn c : columns) {
+                System.out.println(
+                        "    {" + c.getColumnName() + ", "
+                                + c.getColumnType() + ", "
+                                + (c.isPrimaryKey() ? "true}" : "false}"));
+            }
+        }
     }
 }
