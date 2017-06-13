33be3857714a2a0cbbc82d2f9f944b08bf147ff5
noavarice <alex9razinkov@gmail.com>
2017-06-13 15:52:37 +0300

    added checks on column properties

--- src/com/company/parsers/XmlParser.java
+++ src/com/company/parsers/XmlParser.java
@@ -7,11 +7,20 @@ import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class XmlParser {
+    private static final Pattern CHECK_NAME = Pattern.compile("\\w+");
+
+    private static final Set<String> ALLOWED_TYPES = new HashSet<String>() {
+        {
+            add("string");
+            add("integer");
+            add("float");
+            add("date");
+        }
+    };
 
     public static Collection<SqlTable> fromFile(String filePath) throws
             IOException,
@@ -27,6 +36,9 @@ public class XmlParser {
             Element item = (Element)(childNodes.item(i));
             NodeList columnNodes = item.getElementsByTagName("column");
             int columnCount = columnNodes.getLength();
+            if (columnCount == 0) {
+                return null;
+            }
             SqlColumn[] columns = new SqlColumn[columnCount];
             for (int j = 0; j < columnCount; ++j) {
                 if (columnNodes.item(j).getNodeType() != Node.ELEMENT_NODE) {
@@ -35,13 +47,23 @@ public class XmlParser {
                 NamedNodeMap attrs = columnNodes.item(j).getAttributes();
                 String name, type;
                 try {
-                    name = attrs.getNamedItem("name").getNodeValue();
-                    type = attrs.getNamedItem("type").getNodeValue();
+                    name = attrs.getNamedItem("name").getNodeValue().trim();
+                    if (!CHECK_NAME.matcher(name).matches()) {
+                        return null;
+                    }
+                    type = attrs.getNamedItem("type").getNodeValue().trim().toLowerCase();
+                    if (!ALLOWED_TYPES.contains(type)) {
+                        return null;
+                    }
                 } catch (NullPointerException e) {
                     return null;
                 }
-                Node isPk = attrs.getNamedItem("isPrimaryKey");
-                columns[j] = new SqlColumn(name, type, (isPk == null || isPk.getNodeValue().equals("false")) ? false : true);
+                Node isPkNode = attrs.getNamedItem("isPrimaryKey");
+                String isPk = isPkNode == null ? "false" : isPkNode.getNodeValue().toLowerCase();
+                if (!isPk.equals("false") && !isPk.equals("true")) {
+                    return null;
+                }
+                columns[j] = new SqlColumn(name, type, isPk.equals("true"));
             }
             NamedNodeMap attrs = item.getAttributes();
             result.addLast(new SqlTable(attrs.getNamedItem("name").getNodeValue(), Arrays.asList(columns)));
