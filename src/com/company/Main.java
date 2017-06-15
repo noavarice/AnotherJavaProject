9cbe616b20ae74e4ac33aa710e90d1936667f443
noavarice <alex9razinkov@gmail.com>
2017-06-15 13:41:06 +0300

    Main.java changed to test table generating

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,43 @@
 package com.company;
 
-import com.company.models.SqlColumn;
+import com.company.generators.DataGenerator;
 import com.company.models.SqlTable;
 import com.company.parsers.XmlParser;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.xml.sax.SAXException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Properties;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, SAXException {
+    public static void main(String[] args) throws IOException, SAXException, SQLException {
         Collection<SqlTable> tables = XmlParser.fromFile("/home/alexrazinkov/Projects/Java/xml_test");
         if (tables == null) {
             System.out.println("No tables");
             return;
         }
+        Properties props = new Properties();
+        props.load(new FileInputStream(new File("/home/alexrazinkov/Projects/Java/conn")));
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setServerName(props.getProperty("server"));
+        ds.setDatabaseName(props.getProperty("database"));
+        ds.setUser(props.getProperty("username"));
+        ds.setPassword(props.getProperty("password"));
+        Connection conn = ds.getConnection();
+        boolean result;
         for (SqlTable t : tables) {
-            SqlColumn[] columns = t.getTableColumns();
-            System.out.println(t.getTableName());
-            for (SqlColumn c : columns) {
-                System.out.println(
-                        "    {" + c.getColumnName() + ", "
-                                + c.getColumnType() + ", "
-                                + (c.isPrimaryKey() ? "true}" : "false}"));
+            result = DataGenerator.fillTable(conn, t);
+            if (!result) {
+                System.out.println("Failed");
+                return;
             }
         }
+        System.out.println("Succeded");
     }
 }
