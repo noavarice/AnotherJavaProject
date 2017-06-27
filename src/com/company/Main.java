24cc4b3f46a5c31cf17850059b1b8b8164da6398
noavarice <alex9razinkov@gmail.com>
2017-06-28 01:13:13 +0300

    pass file names as command-line arguments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,19 @@
 package com.company;
 
 import com.company.generators.DataGenerator;
-import com.company.generators.DatabaseGenerationException;
-import org.xml.sax.SAXException;
-
-import javax.management.modelmbean.XMLParseException;
-import javax.xml.crypto.dsig.XMLSignatureException;
-import java.io.IOException;
-import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws
-            IOException,
-            SAXException,
-            SQLException,
-            XMLSignatureException,
-            XMLParseException,
-            DatabaseGenerationException
+    private static final int ARGS_COUNT = 2;
+
+    public static void main(String[] args)
     {
-        String connectionPropertiesFilePath = "/home/alexrazinkov/Projects/Java/conn";
-        String tableDeclarationsFilePath = "/home/alexrazinkov/Projects/Java/xml_test";
+        if (args.length != ARGS_COUNT) {
+            System.out.println("Invalid arguments count");
+        }
         try {
-            DataGenerator.generateDatabase(connectionPropertiesFilePath, tableDeclarationsFilePath);
-        } catch (XMLSignatureException e) {
-            System.out.println(e.getMessage());
-        } catch (XMLParseException e) {
+            DataGenerator.generateDatabase(args[0], args[1]);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
